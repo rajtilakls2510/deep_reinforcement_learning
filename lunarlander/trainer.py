52bbbca4d0bf227e0c277652ff824b7edb0d11ec
Rajtilak Pal <rajtilakls2510@gmail.com>
2022-05-18 16:26:56 +0530

    Finished Lunar Lander

--- lunarlander/trainer.py
+++ lunarlander/trainer.py
@@ -24,39 +24,39 @@ optimizer = Adam(learning_rate=0.0005)
 metric = AvgTotalReward(os.path.join(AGENT_PATH, "train_metric"))
 
 # Start training from scratch
+# driver_algorithm = DeepQLearning(
+#     q_network,
+#     optimizer,
+#     discount_factor=0.999,
+#     exploration=1,
+#     min_exploration=0.01,
+#     exploration_decay=1.01,
+#     exploration_decay_after=1,
+#     update_target_after_steps=1_000,
+#     replay_size=1_00_000
+# )
+#
+# agent = Agent(interpreter, driver_algorithm)
+# for i in range(2_00):
+#     print("Training Iteration:", i)
+#     agent.train(initial_episode=10*i, episodes=10, metric=metric, batch_size=64)
+#     agent.save(AGENT_PATH)
+# interpreter.close()
+
+# Load agent and train (change exploration param)
 driver_algorithm = DeepQLearning(
-    q_network,
-    optimizer,
-    discount_factor=0.999,
-    exploration=1,
-    min_exploration=0.01,
-    exploration_decay=1.01,
+    optimizer=optimizer,
+    exploration=0.08,
+    min_exploration=0.0,
+    exploration_decay=1.25,
     exploration_decay_after=1,
     update_target_after_steps=1_000,
     replay_size=1_00_000
 )
-
 agent = Agent(interpreter, driver_algorithm)
-for i in range(2_00):
-    print("Training Iteration:", i)
-    agent.train(initial_episode=10*i, episodes=10, metric=metric, batch_size=64)
+agent.load(AGENT_PATH)
+for i in range(25, 1_00):
+    print("Training Iteration: ", i)
+    agent.train(initial_episode=10 * i, episodes=10, metric=metric, batch_size=64)
     agent.save(AGENT_PATH)
 interpreter.close()
-
-# Load agent and train (change exploration param)
-# driver_algorithm = DeepQLearning(
-#     optimizer=optimizer,
-#     exploration=0.3,
-#     min_exploration=0.05,
-#     exploration_decay=1.01,
-#     exploration_decay_after=50,
-#     update_target_after_steps=1_000,
-#     replay_size=1_00_000
-# )
-# agent = Agent(interpreter, driver_algorithm)
-# agent.load(AGENT_PATH)
-# for i in range(5_8, 2_00):
-#     print("Training Iteration: ", i)
-#     agent.train(initial_episode=10 * i, episodes=10, metric=metric, batch_size=64)
-#     agent.save(AGENT_PATH)
-# interpreter.close()
