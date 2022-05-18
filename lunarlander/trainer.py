bc34a6be0b244475f007fa92e2a4379885f9c3c2
Rajtilak Pal <rajtilakls2510@gmail.com>
2022-05-18 21:32:10 +0530

    Lunar Lander: Tried a couple of things and did not work

--- lunarlander/trainer.py
+++ lunarlander/trainer.py
@@ -9,7 +9,7 @@ from deep_rl.analytics import AvgTotalReward
 from lunarlander.interface import LunarLanderTerminal, LunarLanderInterpreter
 
 interpreter = LunarLanderInterpreter(LunarLanderTerminal(gym.make("LunarLander-v2")))
-AGENT_PATH = "lunar_lander_agent2"
+AGENT_PATH = "lunar_lander_agent"
 
 # Q Network
 input = Input(shape=(8,))
@@ -25,37 +25,37 @@ q_network.compile(optimizer=optimizer)
 metric = AvgTotalReward(os.path.join(AGENT_PATH, "train_metric"))
 
 # Start training from scratch
-# driver_algorithm = DeepQLearning(
-#     q_network,
-#     discount_factor=0.999,
-#     exploration=1,
-#     min_exploration=0.01,
-#     exploration_decay=1.01,
-#     exploration_decay_after=1,
-#     update_target_after_steps=1_000,
-#     replay_size=1_00_000
-# )
-#
-# agent = Agent(interpreter, driver_algorithm)
-# for i in range(25):
-#     print("Training Iteration:", i)
-#     agent.train(initial_episode=10*i, episodes=10, metric=metric, batch_size=64)
-#     agent.save(AGENT_PATH)
-# interpreter.close()
-
-# Load agent and train (change exploration param)
 driver_algorithm = DeepQLearning(
-    exploration=0.224,
-    min_exploration=0.005,
+    q_network,
+    discount_factor=0.999,
+    exploration=1,
+    min_exploration=0.01,
     exploration_decay=1.01,
     exploration_decay_after=1,
     update_target_after_steps=1_000,
     replay_size=1_00_000
 )
+
 agent = Agent(interpreter, driver_algorithm)
-agent.load(AGENT_PATH)
-for i in range(15, 1_00):
-    print("Training Iteration: ", i)
-    agent.train(initial_episode=10 * i, episodes=10, metric=metric, batch_size=64)
+for i in range(25):
+    print("Training Iteration:", i)
+    agent.train(initial_episode=10*i, episodes=10, metric=metric, batch_size=64)
     agent.save(AGENT_PATH)
 interpreter.close()
+
+# Load agent and train (change exploration param)
+# driver_algorithm = DeepQLearning(
+#     exploration=0.224,
+#     min_exploration=0.005,
+#     exploration_decay=1.01,
+#     exploration_decay_after=1,
+#     update_target_after_steps=1_000,
+#     replay_size=1_00_000
+# )
+# agent = Agent(interpreter, driver_algorithm)
+# agent.load(AGENT_PATH)
+# for i in range(15, 1_00):
+#     print("Training Iteration: ", i)
+#     agent.train(initial_episode=10 * i, episodes=10, metric=metric, batch_size=64)
+#     agent.save(AGENT_PATH)
+# interpreter.close()
