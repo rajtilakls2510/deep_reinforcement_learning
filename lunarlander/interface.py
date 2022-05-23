d37bc5247ded4fd708f3202e06dc52dda1aad96a
Rajtilak Pal <rajtilakls2510@gmail.com>
2022-05-23 22:48:54 +0530

    Tested Q network with conv nets in lunar lander

--- lunarlander/interface.py
+++ lunarlander/interface.py
@@ -1,6 +1,6 @@
 from deep_rl.agent import Terminal, Interpreter
-from tensorflow import random, int32
-import numpy as np
+from tensorflow import random, int32, zeros
+from tensorflow.keras.layers import Rescaling, Resizing
 
 
 class LunarLanderTerminal(Terminal):
@@ -29,7 +29,30 @@ class LunarLanderInterpreter(Interpreter):
 
     def __init__(self, terminal: Terminal):
         super(LunarLanderInterpreter, self).__init__(terminal)
-        self.last_action = 0
 
     def get_randomized_action(self):
         return random.uniform(shape=(), maxval=4, dtype=int32).numpy()
+
+
+class LunarLanderImageInterpreter(LunarLanderInterpreter):
+    def __init__(self, terminal: Terminal, frame_buffer_size=5, preprocessed_frame_shape=(80,80,3)):
+        super(LunarLanderImageInterpreter, self).__init__(terminal)
+        self.frame_buffer = [zeros(shape=preprocessed_frame_shape).numpy() for _ in range(frame_buffer_size)]
+        self.frame_buffer_size = frame_buffer_size
+        self.rescale = Rescaling(scale=1.0/255)
+        self.resize = Resizing(width=preprocessed_frame_shape[0], height=preprocessed_frame_shape[1])
+
+    def observe(self):
+        frame, state, self.env_finished, reward = self.terminal.observation()
+        self.steps_taken += 1
+        preprocessed_state = self.state_preprocessing(frame)
+        reward = self.calculate_reward(state, preprocessed_state, reward)
+        if self.env_finished:
+            self.steps_taken = 0
+        return preprocessed_state, reward, frame
+
+    def state_preprocessing(self, frame):
+        if len(self.frame_buffer) >= self.frame_buffer_size:
+            self.frame_buffer.pop(0)
+        self.frame_buffer.append(self.resize(self.rescale(frame)).numpy())
+        return self.frame_buffer
